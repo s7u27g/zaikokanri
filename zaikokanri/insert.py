c30b8d58fd68468e56c833ebb2f8c901227d642d
s7u27g <s7u27astr0@gmail.com>
2023-05-28 20:27:46 +0900

    add insert.py

---
+++ zaikokanri/insert.py
@@ -0,0 +1,54 @@
+# import kakushin's function
+
+col_dict = {
+    'name': 'str',
+    'count': 'int',
+    # '': '',
+    # '': '',
+}
+
+def make_insert_query(columns):
+    '''
+    columns: dict
+    '''
+    pass
+
+def check_database():
+    pass
+
+def make_database():
+    pass
+
+def check_columns():
+    ### get_columns()
+    ### conpare with col_dict
+    pass
+
+def add_columns():
+    pass
+
+def get_newer_record():
+    '''
+    use kakushin's function
+    '''
+    pass
+
+def make_record():
+    pass
+
+def get_columns():
+    pass
+
+def _insert_record():
+    pass
+
+def insert_record():
+    ### check_database()
+    ### make_database()
+    ### check_columns()
+    ### add_columns()
+    ### get_newer_record()
+    ### make_record()
+    ### _insert_record()
+    ### make_insert_query()
+    pass
